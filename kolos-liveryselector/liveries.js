7be4bf62089786b483805971d2dff039ee380a01
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2024-03-14 14:26:59 -0700

    Create liveries.js

---
+++ kolos-liveryselector/liveries.js
@@ -0,0 +1,4 @@
+    var scriptML = document.createElement("script");
+    scriptML.src = "https://raw.githack.com/kolos26/GEOFS-LiverySelector/main/main.js";
+    document.body.appendChild(scriptML);
+    localStorage.favorites = "";
